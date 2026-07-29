397b36c5563771549ff43f2d7424775145b458b2
Nicole Valencia <lvalenc5@students.kennesaw.edu>
2026-07-28 20:15:43 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,38 @@
+# Smart Booking
+
+
+from datetime import datetime
+
+class booking 
+
+def __init__(self, title, date, consultant):
+        self.title = title
+        self.date  = date
+        self.consultant = consultant
+        
+ # double booking 
+def show_booking(self):
+        print("Appointment: {self.title}")
+        print("Date: {self.date}")
+        print("Consultant: {self.consultant}")
+
+# Create appointment
+booking1 = Booking(
+    "Hair Appointment",
+    "05/10/2026",
+    "Clarissa"
+        
+def add_booking(self, new_booking):
+ for booking in self.bookings:
+ if booking.consultant == new_booking.consultant:
+ if booking.overlaps(new_booking):
+print(" Double booking detected!")
+ return False
+
+def find_date
+ return self.date
+
+def find_consultant 
+return self.consultant
+
+
